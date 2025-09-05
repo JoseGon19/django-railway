6741634add670c57ea19a906dac1030b55c5381e
JoseGon19 <103158572+JoseGon19@users.noreply.github.com>
2025-09-05 14:18:21 -0600

    allowed host

--- centromac/settings.py
+++ centromac/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tmxg=#3f2(vo=!l5=5#==6fb5n8#j-@e+$4xcl323ltop$xs)u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://django-railway-production-68c3.up.railway.app/']
+ALLOWED_HOSTS = ['localhost','django-railway-production-671a.up.railway.app']
 
 
 # Application definition
